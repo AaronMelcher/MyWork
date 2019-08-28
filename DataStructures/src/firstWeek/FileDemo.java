802fa82bba9291512eed206e324ac6cd1f0f363b
Aaron Melcher <amelch130@gmail.com>
2019-08-28 12:40:52 -0500

    Did more file reading stuff

--- DataStructures/src/firstWeek/FileDemo.java
+++ DataStructures/src/firstWeek/FileDemo.java
@@ -3,38 +3,32 @@ package firstWeek;
 import java.util.*;
 import java.io.*;
 
-public class FileDemo
-{
-    public static void main(String[] args) throws IOException
-    {
-        Scanner inputStream = null;
-        PrintWriter outputStream = null;
+public class FileDemo {
+	public static void main(String[] args) throws IOException {
+		Scanner inputStream = null;
+		PrintWriter outputStream = null;
 
-        Scanner console = new Scanner(System.in);
-        System.out.print("Input file: ");
+		Scanner console = new Scanner(System.in);
+		System.out.print("Input file: ");
 		String inputFileName = console.next();
 		System.out.print("Output file: ");
-      	String outputFileName = console.next();
+		String outputFileName = console.next();
 
-        inputStream =
-              new Scanner(new FileInputStream(inputFileName));
-           outputStream = new PrintWriter(
-                           new FileOutputStream(outputFileName));
+		inputStream = new Scanner(new FileInputStream(inputFileName));
+		outputStream = new PrintWriter(new FileOutputStream(outputFileName));
 
-        String line = null;
-        int count = 0;
+		String line = null;
+		int count = 0;
 
-        while (inputStream.hasNextLine( ))
-        {
-            line = inputStream.nextLine( );
-            count++;
-            outputStream.println(count + ":" + line);
-        }
+		while (inputStream.hasNextLine()) {
+			line = inputStream.nextLine();
+			count++;
+			outputStream.println(count + ":" + line);
+		}
 
-        inputStream.close( );
-        outputStream.close( );
-        console.close();
-    }
+		inputStream.close();
+		outputStream.close();
+		console.close();
+	}
 
 }
-
