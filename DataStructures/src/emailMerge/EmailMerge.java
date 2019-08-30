1e3466b587302f9bf292dcdada51d6faa56c668f
Aaron Melcher <amelch130@gmail.com>
2019-08-30 10:55:01 -0500

    Got File to read into ecplise, working on merging new stuff into one
    Issues with getting strings to switch and stuff like that

--- DataStructures/src/emailMerge/EmailMerge.java
+++ DataStructures/src/emailMerge/EmailMerge.java
@@ -8,11 +8,26 @@ public class EmailMerge {
 	public static void main(String[] args) throws IOException {
 		File template = new File("src/emailMerge/template.txt");
 		Scanner input = new Scanner(template);
-		String[] words = input.nextLine().split(" ");
+		File people = new File("src/emailMerge/people.txt");
+		Scanner input2 = new Scanner(people);
+		String temp = new String();
 		
-		for(String temp : words) {
-			System.out.println(temp);
+		while(input.hasNextLine()) {
+			temp += input.nextLine() + " ";
 		}
+		
+		String[] peopleTest = input2.nextLine().split(" ");
+		
+		String[] words = temp.split(" ");
+		
+		for(String i : words) {
+			if(i.equals("<<N>>,")){
+				i = peopleTest[0];
+			}
+			System.out.println(words[1]);
+		}
+		
+		
 		input.close();
 	}
 }
