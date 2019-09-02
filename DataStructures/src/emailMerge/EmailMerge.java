2d7e50811cd3b588e77f68df2d7c35dfffd6a734
Aaron Melcher <amelch130@gmail.com>
2019-09-02 10:19:59 -0500

    Updates to emailmerge

--- DataStructures/src/emailMerge/EmailMerge.java
+++ DataStructures/src/emailMerge/EmailMerge.java
@@ -6,10 +6,8 @@ import java.io.*;
 public class EmailMerge {
 
 	public static void main(String[] args) throws IOException {
-		File template = new File("src/emailMerge/template.txt");
-		Scanner input = new Scanner(template);
-		File people = new File("src/emailMerge/people.txt");
-		Scanner input2 = new Scanner(people);
+		Scanner input = new Scanner(new File("src/emailMerge/template.txt"));
+		Scanner input2 = new Scanner(new File("src/emailMerge/people.txt"));
 		String temp = new String();
 
 		while (input.hasNextLine()) {
@@ -18,11 +16,27 @@ public class EmailMerge {
 
 		String[] words = temp.split(" ");
 		ArrayList<String> names = new ArrayList<String>();
-		do {
-			String info = input.nextLine();
+
+		while (input2.hasNext()) {
+			String[] fin = new String[words.length];
+			String[] info = input.nextLine().split(" ");
+			names.add(info[0]);
+			for (int i = 0; i < words.length; i++) {
+				if (words[i].equals("<<N>>,")) {
+					fin[i] = info[0];
+				} else if (words[i].equals("<<A>>")) {
+					fin[i] = info[1];
+				} else if (words[i].equals("<<G>>,")) {
+					fin[i] = info[2];
+				} else {
+					fin[i] = words[i];
+				}
+			}
 			
-		} while (input2.hasNext());
+			
+		}
 
 		input.close();
+		input2.close();
 	}
 }
