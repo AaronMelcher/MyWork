47572b8e4b63c9d594142c5b528db640d828f086
Aaron Melcher <amelch130@gmail.com>
2019-09-20 14:11:21 -0500

    Did the stats thingy in the CLI file

--- DataStructures/src/bookFiles/ch02/apps/PFixCLI.java
+++ DataStructures/src/bookFiles/ch02/apps/PFixCLI.java
@@ -14,11 +14,12 @@ import bookFiles.ch02.postfix.*;
 public class PFixCLI {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		Scanner inputs;
 
 		String expression = null; // expression to be evaluated
 		final String STOP = "X"; // indicates end of input
 		int result; // result of evaluation
-		int smallest, largest, count, total;
+		int smallest, largest, count = 0, total = 0, num;
 
 		while (!STOP.equals(expression)) {
 			// Get next expression to be processed.
@@ -29,9 +30,33 @@ public class PFixCLI {
 				// Obtain and output result of expression evaluation.
 				try {
 					result = PostFixEvaluator.evaluate(expression);
+					inputs = new Scanner(expression);
+					largest = inputs.nextInt();
+					smallest = largest;
+					count++;
+					total += largest;
+					while (inputs.hasNext()) {
+						if (inputs.hasNextInt()) {
+							num = inputs.nextInt();
+							if (num > largest)
+								largest = num;
+							else if (num < smallest)
+								smallest = num;
+							total += num;
+							count++;
+						}
+					}
 
 					// Output result.
 					System.out.println("Result = " + result);
+					System.out.println("****STATISTICS****");
+					System.out.println("Largest: " + largest);
+					System.out.println("Smallest: " + smallest);
+					System.out.println("Count: " + count);
+					System.out.println("Average: " + total / count);
+					count = 0;
+					total = 0;
+
 				} catch (PostFixException error) {
 					// Output error message.
 					System.out.println("Error in expression - " + error.getMessage());
