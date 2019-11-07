9cf4820830819780eb0907a20ed7e826602ee263
Aaron <amelch130@gmail.com>
2019-11-06 18:38:59 -0800

    ow

--- DataStructures/src/collections/Assign5/WordFrequency.java
+++ DataStructures/src/collections/Assign5/WordFrequency.java
@@ -8,23 +8,29 @@ public class WordFrequency {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner document = new Scanner(new File("src/bookFiles/input/Keywords.txt"));
         HashSet<String> words = new HashSet<String>();
+        ArrayList<String> allWords = new ArrayList<String>();
+        int count = 0;
+        String temp = "";
 
-        while(document.hasNext())
-            if(words.add(document.next()))
-            words.add(document.next());
+        while (document.hasNext()){
+            temp = document.next();
+            if(words.add(temp.trim()))
+                words.add(temp.trim());
+            allWords.add(temp);
+        }
 
         HashMap<String, Integer> numOfWords = new HashMap<String, Integer>();
 
         for(String word : words){
-            int count = 0;
-            while(document.hasNext()){
-                if(document.next().equals(word))
+            for(String check : allWords){
+                if(word.equals(check))
                     count++;
             }
             numOfWords.put(word, count);
+            count = 0;
         }
-        
-        for(Map.Entry<String, Integer> keyAndValue : numOfWords.entrySet()){
+
+        for (Map.Entry<String, Integer> keyAndValue : numOfWords.entrySet()) {
             System.out.println(keyAndValue.getKey() + "->" + keyAndValue.getValue());
         }
         document.close();
