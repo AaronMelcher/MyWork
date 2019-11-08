59b6ea5c794d4f09d16d8938e9374164496eb0c0
Aaron <amelch130@gmail.com>
2019-11-07 21:21:07 -0800

    yay

--- DataStructures/src/collections/Assign5/WordFrequency.java
+++ DataStructures/src/collections/Assign5/WordFrequency.java
@@ -2,52 +2,41 @@ package collections.assign5;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
 
 public class WordFrequency {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner document = new Scanner(new File("src/bookFiles/input/Keywords.txt"));
-        HashSet<String> words = new HashSet<String>();
-        int count = 0;
+        Scanner doc = new Scanner(new File("src/bookFiles/input/Keywords.txt"));
+        String toSplit = "";
 
-        while (document.hasNext()) {
-            if (words.add(document.next()))
-                words.add(document.next());
+        while(doc.hasNext()){
+            toSplit += doc.next() + " ";
         }
-        document.reset();
+
+        String[] docWords = toSplit.split(" ");
+        int count = 0;
+        HashSet<String> word =  new HashSet<String>();
+
+        for(String toAdd : docWords)
+            if(word.add(toAdd))
+                word.add(toAdd);
+
         HashMap<String, Integer> numOfWords = new HashMap<String, Integer>();
 
-        for (String word : words) {
-            while (document.hasNext()) {
-                if (word.equals(document.next()))
+        for(String toAdd : word){
+            for(String check : docWords){
+                if(toAdd.equals(check))
                     count++;
             }
-            numOfWords.put(word, count);
-            document.reset();
+            numOfWords.put(toAdd, count);
             count = 0;
         }
+        for(Map.Entry<String, Integer> keyAndValue : numOfWords.entrySet())
+            System.out.println(keyAndValue.getKey() + "->" + keyAndValue.getValue());
 
-        numOfWords = sortByValue(numOfWords);
-        System.out.println(numOfWords);
-        document.close();
-    }
-
-    public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm) {
-        // Create a list from elements of HashMap
-        List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(hm.entrySet());
-
-        // Sort the list
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return (o1.getValue()).compareTo(o2.getValue());
-            }
-        });
-
-        // put data from sorted list to hashmap
-        HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
-        for (Map.Entry<String, Integer> aa : list) {
-            temp.put(aa.getKey(), aa.getValue());
-        }
-        return temp;
+        doc.close();
     }
 }
