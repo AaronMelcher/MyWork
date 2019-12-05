ae31e5555ae1f0fbff60a4412261417156178f2e
Aaron <amelch130@gmail.com>
2019-12-04 18:02:36 -0600

    :=)
    DS is almost over

--- DataStructures/src/assign6/TestTree.java
+++ DataStructures/src/assign6/TestTree.java
@@ -6,16 +6,10 @@ public class TestTree {
 		
 		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
 
-		
 		tree.add(50);
-		tree.add(40);
-		tree.add(30);
-		tree.add(45);
-		tree.add(60);
-		tree.add(90);
-		tree.add(80);
-		tree.add(100);
-		tree.add(95);
+		tree.add(20);
+		tree.add(25);
+		tree.add(26);
 
 		tree.printSideways();
 		System.out.println();
